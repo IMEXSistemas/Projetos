decd8bd9e88e3e4d45103a905d7e61b16dbc41af
IMEX Sistemas <contato@imexsistemas.com.br>
2018-01-28 10:06:41 -0200

    28/01/2018 10:04

--- IMEXSistema/IMEXSistema/IMEXSistema/Modulos/Lote/FrmLote.cs
+++ IMEXSistema/IMEXSistema/IMEXSistema/Modulos/Lote/FrmLote.cs
@@ -29,7 +29,7 @@ namespace BmsSoftware.Modulos.Cadastros
 
         public FrmLote()
         {
-			//Teste git - 28/01/2018 - 10:03
+			//Teste git - 28/01/2018 - 10:04
             InitializeComponent();
             RegisterFocusEvents(this.Controls);
         }
